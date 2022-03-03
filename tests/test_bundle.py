39e06cbb2dc0de7d9fd096c0f7d2464f90137415
Andrew Scribner <ca.scribner+1@gmail.com>
2022-03-03 16:23:06 -0500

    feat: add test for k8s access, refactor tests
    * add a test that checks whether kubectl actually finds a config file with a context, returning a useful error if it doesn't.  To run tox we need to do `KUBECONFIG=something tox -e ...` which is a different pattern than normal and that does not fail intuitively when the `KUBECONFIG` is missing.  This change helps to warn people about that and suggests some fixes
    * broke the build_and_deploy test up, splitting it into an actual build_and_deploy and a separate test of the bookinfo workload.  This way users can execute the tests separately, and also it just makes more atomic tests

--- tests/test_bundle.py
+++ tests/test_bundle.py
@@ -1,9 +1,9 @@
 import json
 import logging
-from asyncio import sleep
 
 import aiohttp
 import pytest
+import yaml
 from bs4 import BeautifulSoup as BS
 from pytest_operator.plugin import OpsTest
 
@@ -11,21 +11,46 @@ log = logging.getLogger(__name__)
 
 
 @pytest.mark.abort_on_fail
+async def test_kubectl_access(ops_test: OpsTest):
+    """Fails if kubectl not available or if no cluster context exists"""
+    _, stdout, _ = await ops_test.run(
+        'kubectl',
+        'config',
+        'view',
+        check=True,
+        fail_msg="Failed to execute kubectl - is kubectl installed?",
+    )
+
+    # Check if kubectl has a context, failing if it does not
+    kubectl_config = yaml.safe_load(stdout)
+    error_message = (
+        "Found no kubectl contexts - did you populate KUBECONFIG?  Ex:"
+        " 'KUBECONFIG=/home/runner/.kube/config tox ...' or"
+        " 'KUBECONFIG=/home/runner/.kube/config tox ...'"
+    )
+    assert kubectl_config["contexts"] is not None, error_message
+
+    await ops_test.run(
+        "kubectl",
+        "get",
+        "pods",
+        check=True,
+        fail_msg="Failed to do a simple kubectl task - is KUBECONFIG properly configured?"
+    )
+
+
+@pytest.mark.abort_on_fail
 async def test_deploy_bundle(ops_test: OpsTest):
     await ops_test.deploy_bundle(serial=True, extra_args=['--trust'])
 
-    await ops_test.model.wait_for_idle(timeout=60 * 10)
-    await ops_test.run(
-        'kubectl',
-        'wait',
-        '--for=condition=available',
-        'deployment',
-        '--all',
-        '--all-namespaces',
-        '--timeout=5m',
-        check=True,
+    await ops_test.model.wait_for_idle(
+        status="active",
+        raise_on_blocked=True,
+        timeout=60 * 10,
     )
 
+
+async def test_deploy_bookinfo_example(ops_test: OpsTest):
     root_url = 'https://raw.githubusercontent.com/istio/istio/release-1.11/samples/bookinfo'
     await ops_test.run(
         'kubectl',
