922900041877a431d5dec285deab0d629dd3b332
Daniela Plascencia <daniela.plascencia@canonical.com>
2022-04-04 08:43:54 -0500

    istio-pilot/tests/unit: modifies tests to reflect change in prev commit

--- charms/istio-pilot/tests/unit/conftest.py
+++ charms/istio-pilot/tests/unit/conftest.py
@@ -41,24 +41,16 @@ def mocked_client(mocker):
     yield client
 
 
-# TODO: once we extract the _get_gateway_address method
-# from the charm code, this bit won't be necessary
-@pytest.fixture(autouse=True)
-def mocked_charm_client(mocker):
-    client = mocker.patch("charm.Client")
-    yield client
-
-
 # Similar to what is done for list, but for get
 # and just returning the status attribute
 @pytest.fixture(autouse=True)
-def mocked_get(mocked_charm_client, mocker):
+def mocked_get(mocked_client, mocker):
     mocked_resource_obj = mocker.MagicMock()
     mocked_metadata = mocker.MagicMock()
     mocked_metadata.status = 'status'
     mocked_resource_obj.metadata = mocked_metadata
 
-    mocked_charm_client.return_value.get.side_effect = mocked_resource_obj
+    mocked_client.return_value.get.side_effect = mocked_resource_obj
 
 
 # autouse to ensure we don't accidentally call out, but
