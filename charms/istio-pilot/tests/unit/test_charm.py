3582953dc5a4e6d87405f4e925470df99c295912
Andrew Scribner <ca.scribner+1@gmail.com>
2023-04-11 15:54:49 -0400

    add unit test for event triggers

--- charms/istio-pilot/tests/unit/test_charm.py
+++ charms/istio-pilot/tests/unit/test_charm.py
@@ -1,1 +1,85 @@
-raise NotImplementedError("TODO: add unit tests")
+from unittest.mock import Mock
+
+import pytest
+from ops.charm import RelationCreatedEvent, RelationChangedEvent, RelationBrokenEvent
+from ops.testing import Harness
+
+from charm import Operator
+
+
+# TODO: Fixtures to block lightkube
+# TODO: Fixtures to block istioctl
+
+
+class TestCharmEvents:
+    """Test cross-cutting charm behavior.
+
+    TODO: Explain this better
+    """
+    def test_events(self, harness, mocker):
+        harness.begin()
+        mocked_install = mocker.patch("charm.Operator.install")
+        mocked_remove = mocker.patch("charm.Operator.remove")
+        mocked_upgrade_charm = mocker.patch("charm.Operator.upgrade_charm")
+        mocked_reconcile = mocker.patch("charm.Operator.reconcile")
+
+        RelationCreatedEvent
+        harness.charm.on.install.emit()
+        assert_called_once_and_reset(mocked_install)
+
+        harness.charm.on.remove.emit()
+        assert_called_once_and_reset(mocked_remove)
+
+        harness.charm.on.upgrade_charm.emit()
+        assert_called_once_and_reset(mocked_upgrade_charm)
+
+        exercise_relation(harness, "gateway-info")
+        assert mocked_reconcile.call_count == 1
+        assert isinstance(mocked_reconcile.call_args_list[0][0][0], RelationCreatedEvent)
+        mocked_reconcile.reset_mock()
+
+        exercise_relation(harness, "istio-pilot")
+        assert mocked_reconcile.call_count == 3
+        assert isinstance(mocked_reconcile.call_args_list[0][0][0], RelationCreatedEvent)
+        assert isinstance(mocked_reconcile.call_args_list[1][0][0], RelationChangedEvent)
+        assert isinstance(mocked_reconcile.call_args_list[2][0][0], RelationBrokenEvent)
+        mocked_reconcile.reset_mock()
+
+        exercise_relation(harness, "ingress")
+        assert mocked_reconcile.call_count == 2
+        assert isinstance(mocked_reconcile.call_args_list[0][0][0], RelationChangedEvent)
+        assert isinstance(mocked_reconcile.call_args_list[1][0][0], RelationBrokenEvent)
+        mocked_reconcile.reset_mock()
+
+        exercise_relation(harness, "ingress-auth")
+        assert mocked_reconcile.call_count == 2
+        assert isinstance(mocked_reconcile.call_args_list[0][0][0], RelationChangedEvent)
+        assert isinstance(mocked_reconcile.call_args_list[1][0][0], RelationBrokenEvent)
+        mocked_reconcile.reset_mock()
+
+
+# Fixtures
+@pytest.fixture
+def harness():
+    return Harness(Operator)
+
+
+# Helpers
+def assert_called_once_and_reset(mock: Mock):
+    mock.assert_called_once()
+    mock.reset_mock()
+
+
+def exercise_relation(harness, relation_name):
+    """Exercises a relation by creating, joining, updating, departing, and breaking it."""
+    other_app = "other"
+    other_unit = f"{other_app}/0"
+    rel_id = harness.add_relation(relation_name, other_app)
+    harness.add_relation_unit(rel_id, other_unit)
+    harness.update_relation_data(rel_id, other_app, {"some_key": "some_value"})
+    harness.remove_relation_unit(rel_id, other_unit)
+    harness.remove_relation(rel_id)
+
+
+def test_assert_this_suite_is_incomplete():
+    raise NotImplementedError("This test suite is incomplete")
