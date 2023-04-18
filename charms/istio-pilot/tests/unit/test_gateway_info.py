8f94cb8f5e74e42f2030b6a7eaa9c05c10ccf07c
Daniela Plascencia <daniela.plascencia@canonical.com>
2023-04-18 17:46:21 +0200

    [skip] accept suggestions

--- charms/istio-pilot/tests/unit/test_gateway_info.py
+++ charms/istio-pilot/tests/unit/test_gateway_info.py
@@ -10,40 +10,38 @@ from charms.istio_pilot.v0.istio_gateway_info import (
 from ops.charm import CharmBase
 from ops.testing import Harness
 
-DEFAULT_RELATION_NAME = "gateway-info"
-DEFAULT_INTERFACE_NAME = "istio-gateway-info"
-TEST_CHARM_META = f"""
+REQUIRER_CHARM_META = f"""
 name: test-charm
 requires:
-  {DEFAULT_RELATION_NAME}:
-    interface: {DEFAULT_INTERFACE_NAME}
+  test-relation:
+    interface: test-interface
 """
 
 class GenericCharm(CharmBase):
     pass
 
 @pytest.fixture()
-def generic_charm_harness():
-    return Harness(GenericCharm, meta=TEST_CHARM_META)
+def requirer_charm_harness():
+    return Harness(GenericCharm, meta=REQUIRER_CHARM_META)
 
-def test_get_relation_data_passes(generic_charm_harness, mocker):
+def test_get_relation_data_passes(requirer_charm_harness, mocker):
     """Assert the relation data is as expected."""
     # Initial configuration
-    generic_charm_harness.set_model_name("test-model")
-    generic_charm_harness.begin()
-    generic_charm_harness.set_leader(True)
-    relation_id = generic_charm_harness.add_relation(DEFAULT_RELATION_NAME, "app")
+    requirer_charm_harness.set_model_name("test-model")
+    requirer_charm_harness.begin()
+    requirer_charm_harness.set_leader(True)
+    relation_id = requirer_charm_harness.add_relation("test-relation", "app")
 
     # Instantiate GatewayRequirer class
-    generic_charm_harness.charm.gateway_requirer = GatewayRequirer(generic_charm_harness.charm)
+    requirer_charm_harness.charm.gateway_requirer = GatewayRequirer(requirer_charm_harness.charm, relation_name="test-relation")
 
     # Add and update relation 
     expected_relation_data = {"gateway_name": "name", "gateway_namespace": "namespace"}
-    generic_charm_harness.add_relation_unit(relation_id, "app/0")
-    generic_charm_harness.update_relation_data(relation_id, "app", expected_relation_data)
+    requirer_charm_harness.add_relation_unit(relation_id, "app/0")
+    requirer_charm_harness.update_relation_data(relation_id, "app", expected_relation_data)
 
     # Get the relation data
-    actual_relation_data = generic_charm_harness.charm.gateway_requirer.get_relation_data()
+    actual_relation_data = requirer_charm_harness.charm.gateway_requirer.get_relation_data()
 
     # Assert returns dictionary with expected values
     assert actual_relation_data == expected_relation_data
