3974636a85fa4d346a6ea10ffad34746cca1cbb2
i-chvets <113444075+i-chvets@users.noreply.github.com>
2023-09-05 12:15:18 -0400

    fix: updated get-images script (#327)
    Summary of changes:
    - Updated get images script to dynamically extract Istio images

--- tools/get-images.sh
+++ tools/get-images.sh
@@ -2,12 +2,13 @@
 #
 # This script returns list of container images that are managed by this charm and/or its workload
 #
-# static list
-STATIC_IMAGE_LIST=(
-docker.io/istio/pilot:1.17.3
-)
 # dynamic list
 IMAGE_LIST=()
-IMAGE_LIST+=($(grep image charms/istio-gateway/src/manifest.yaml | awk '{print $2}' | sort --unique))
-printf "%s\n" "${STATIC_IMAGE_LIST[@]}"
-printf "%s\n" "${IMAGE_LIST[@]}"
+IMAGE_LIST+=($(yq '.[] | .proxy-image.default' ./charms/istio-gateway/config.yaml))
+PROXY_IMAGE=$(yq '.[] | .image-configuration.default' ./charms/istio-pilot/config.yaml | yq '.global-proxy-image')
+PILOT_IMAGE=$(yq '.[] | .image-configuration.default' ./charms/istio-pilot/config.yaml | yq '.pilot-image')
+HUB=$(yq '.[] | .image-configuration.default' ./charms/istio-pilot/config.yaml | yq '.global-hub')
+TAG=$(yq '.[] | .image-configuration.default' ./charms/istio-pilot/config.yaml | yq '.global-tag')
+IMAGE_LIST+=($(echo $HUB/$PROXY_IMAGE:$TAG))
+IMAGE_LIST+=($(echo $HUB/$PILOT_IMAGE:$TAG))
+printf "%s\n" "${IMAGE_LIST[@]}" | sort -u
