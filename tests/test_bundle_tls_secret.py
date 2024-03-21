9684d21888d7e0be6afc384345d36e24e3cd55d0
Daniela Plascencia <daniela.plascencia@canonical.com>
2024-03-21 10:38:06 +0100

    skip: update action name

--- tests/test_bundle_tls_secret.py
+++ tests/test_bundle_tls_secret.py
@@ -92,6 +92,6 @@ async def run_save_tls_secret_action(ops_test: OpsTest):
     """Run the save-tls-secret action."""
     istio_pilot_unit = ops_test.model.applications[ISTIO_PILOT].units[0]
     istio_pilot_unit_action = await istio_pilot_unit.run_action(
-        action_name="save-tls-secret", **{"ssl-key": "key", "ssl-crt": "crt"}
+        action_name="set-tls-manually", **{"ssl-key": "key", "ssl-crt": "crt"}
     )
     await ops_test.model.get_action_output(action_uuid=istio_pilot_unit_action.entity_id, wait=120)
