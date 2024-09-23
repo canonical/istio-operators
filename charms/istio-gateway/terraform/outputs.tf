2657b3212d090e85a00e14dc362799fd0abf2b86
mvlassis <manos.vlassis@canonical.com>
2024-09-23 12:55:09 +0300

    Add Terraform charms

---
+++ charms/istio-gateway/terraform/outputs.tf
@@ -0,0 +1,15 @@
+output "app_name" {
+  value = juju_application.istio_gateway.name
+}
+
+output "provides" {
+  value = {
+	metrics_endpoint = "metrics-endpoint"
+  }
+}
+
+output "requires" {
+  value = {
+    istio_pilot = "istio-pilot"
+  }
+}
---
+++ charms/istio-pilot/terraform/outputs.tf
@@ -0,0 +1,20 @@
+output "app_name" {
+  value = juju_application.istio_pilot.name
+}
+
+output "provides" {
+  value = {
+	metrics_endpoint = "metrics-endpoint",
+	grafana_dashboard = "grafana-dashboard",
+	istio_pilot = "istio-pilot",
+	ingress = "ingress",
+	ingress_auth = "ingress-auth",
+	gateway_info = "gatway-info"
+  }
+}
+
+output "requires" {
+  value = {
+    certificates = "certificates"
+  }
+}
