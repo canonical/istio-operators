50a332ce5f225d64f79c6b4a39ba15c781aeb144
Manos Vlassis <57320708+mvlassis@users.noreply.github.com>
2024-09-26 14:14:51 +0300

    Feat: Add Terraform modules (#554)
    * Add Terraform charms
    
    * Update format
    
    * Update action
    
    * Update README.md
    
    * Remove channel from terraform action
    
    * Update default Terraform channel
    
    * Update default Terraform channel

---
+++ charms/istio-gateway/terraform/main.tf
@@ -0,0 +1,13 @@
+resource "juju_application" "istio_gateway" {
+  charm {
+    name     = "istio-gateway"
+    channel  = var.channel
+    revision = var.revision
+  }
+  config    = var.config
+  model     = var.model_name
+  name      = var.app_name
+  resources = var.resources
+  trust     = true
+  units     = 1
+}
