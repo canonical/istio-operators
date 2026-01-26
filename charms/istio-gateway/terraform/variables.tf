d64cf5c372c1df6c451ab2b282495b5acb4e210c
misohu <michal.hucko@canonical.com>
2026-01-26 10:29:14 +0100

    chore: update terraform modules channel to 1.27/stable

--- charms/istio-gateway/terraform/variables.tf
+++ charms/istio-gateway/terraform/variables.tf
@@ -13,7 +13,7 @@ variable "base" {
 variable "channel" {
   description = "Charm channel"
   type        = string
-  default     = "latest/edge"
+  default     = "1.27/stable"
 }
 
 variable "config" {
