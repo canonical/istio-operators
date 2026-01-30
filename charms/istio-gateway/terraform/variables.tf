62291d12c2d7cbf8f8c35f2f09ef5c739fe560ca
MattiaSarti <nmattia1@gmail.com>
2026-01-30 15:34:49 +0100

    ci: restore channel after release

--- charms/istio-gateway/terraform/variables.tf
+++ charms/istio-gateway/terraform/variables.tf
@@ -13,7 +13,7 @@ variable "base" {
 variable "channel" {
   description = "Charm channel"
   type        = string
-  default     = "latest/edge"
+  default     = "1.28/stable"
 }
 
 variable "config" {
